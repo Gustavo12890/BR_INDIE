7aca0110f4dc34b490f700596b01587d8b46712b
Gustavo12890 <gustavooliveiracardoso13@gmail.com>
2023-10-21 16:33:22 -0300

    Estilização da página projeto parte 1

--- pages/projeto.php
+++ pages/projeto.php
@@ -23,7 +23,7 @@
               <a class="nav-link link-hover" aria-current="page" href="home.php">Encontrar Jogos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link link-hover" href="criarJogo.php">Enviar Projetos</a>
+              <a class="nav-link link-hover" href="enviarProjeto.php">Enviar Projetos</a>
             </li>
             <li class="nav-item">
               <a class="nav-link link-hover" href="sobre.php">Sobre</a>
@@ -39,6 +39,7 @@
       </div>
     </nav>
   </header>
+
 <div class="container" style="background-color: #2e0523;">
   <div class="row">
     <div class="col-md-12 col-12 p-0 text-white">
@@ -48,14 +49,14 @@
   <div class="row">
     <div class="col">
       <h1 class="titulo mt-4 mb-4 fs-1 text-center">CELESTE</h1>
-      <p class="text-white">Celeste é um jogo eletrônico de plataforma desenvolvido pelos canadenses Maddy Thorson e Noel Berry, com arte feita pelo estúdio brasileiro MiniBoss. O jogo foi originalmente criado como um protótipo de quatro dias durante uma game jam, e depois foi expandido para o seu lançamento completo. Celeste foi lançado em janeiro de 2018 para Microsoft Windows, Nintendo Switch, PlayStation 4, Xbox One, macOS, e Linux. O jogo recebeu aclamação da crítica e foi premiado como "Melhor Jogo Independente" e "Jogo Mais Impactante" no The Game Awards 2018, além de ter sido indicado para "Jogo do Ano" e "Melhor Trilha Sonora" nesta mesma premiação.</p>
+      <p class="texto-projeto">Celeste é um jogo eletrônico de plataforma desenvolvido pelos canadenses Maddy Thorson e Noel Berry, com arte feita pelo estúdio brasileiro MiniBoss. O jogo foi originalmente criado como um protótipo de quatro dias durante uma game jam, e depois foi expandido para o seu lançamento completo. Celeste foi lançado em janeiro de 2018 para Microsoft Windows, Nintendo Switch, PlayStation 4, Xbox One, macOS, e Linux. O jogo recebeu aclamação da crítica e foi premiado como "Melhor Jogo Independente" e "Jogo Mais Impactante" no The Game Awards 2018, além de ter sido indicado para "Jogo do Ano" e "Melhor Trilha Sonora" nesta mesma premiação.</p>
     </div>
   </div>
   <div class="row mt-5">
     <div class="col-lg-7 col-12 d-flex justify-content-center">
-      <p class="text-white">Celeste é um jogo de plataforma no qual os jogadores controlam uma garota chamada Madeline enquanto ela sobe em uma montanha, evitando vários obstáculos mortais. Além de pular e escalar paredes por um período limitado de tempo, Madeline tem a capacidade de realizar um traço (dash, em inglês) no chão ou no meio do ar nas oito direções cardeais. Este movimento só pode ser realizado uma vez e deve ser reabastecido, seja aterrissando no chão ou atingindo certos objetos, como cristais (embora o jogador receba uma segunda colisão mais tarde no jogo). Ao longo de cada nível, o jogador encontrará mecânicas adicionais, como molas que lançam o jogador ou penas que permitem um breve voo, e objetos mortais como espinhos que matam Madeline (retornando-a para o início da seção). Jogadores menos habilidosos também podem acessar um "Modo de Assistência", onde podem alterar alguns atributos sobre a física do jogo. Algumas delas incluem: traços aéreos infinitos, invencibilidade ou retardar a velocidade do jogo. Escondidos em cada nível estão os morangos opcionais, obtidos através de desafiadoras seções de plataformas ou quebra-cabeças, que afetam levemente o final do jogo dependendo de quantos são coletados. Além disso, existem fitas cassetes que desbloqueiam variações mais difíceis de "B-Side" de certos níveis, e corações de cristal usados ​​para acessar o conteúdo pós-jogo. Vencendo todos os "B-Sides", em seguida, desbloqueia as versões "C-Side", que consiste em muito duras, mas pequenas variações sobre os níveis. Ao limpar todos os "C-Sides", o jogador pode acessar o menu Variantes. O menu Variantes permite que os jogadores alterem a física do jogo de maneira similar ao Modo de Assistência. Algumas dessas configurações "variantes" incluem: acelerar o jogo, 360 graus e baixa fricção em todas as superfícies planas. Essas configurações servem para tornar o jogo mais desafiador ou mais divertido. O protótipo original do "Celeste Classic" para o console Pico-8 também pode ser encontrado como um minigame escondido.</p>
+      <p class="texto-projeto">Celeste é um jogo de plataforma no qual os jogadores controlam uma garota chamada Madeline enquanto ela sobe em uma montanha, evitando vários obstáculos mortais. Além de pular e escalar paredes por um período limitado de tempo, Madeline tem a capacidade de realizar um traço (dash, em inglês) no chão ou no meio do ar nas oito direções cardeais. Este movimento só pode ser realizado uma vez e deve ser reabastecido, seja aterrissando no chão ou atingindo certos objetos, como cristais (embora o jogador receba uma segunda colisão mais tarde no jogo). Ao longo de cada nível, o jogador encontrará mecânicas adicionais, como molas que lançam o jogador ou penas que permitem um breve voo, e objetos mortais como espinhos que matam Madeline (retornando-a para o início da seção). Jogadores menos habilidosos também podem acessar um "Modo de Assistência", onde podem alterar alguns atributos sobre a física do jogo. Algumas delas incluem: traços aéreos infinitos, invencibilidade ou retardar a velocidade do jogo. Escondidos em cada nível estão os morangos opcionais, obtidos através de desafiadoras seções de plataformas ou quebra-cabeças, que afetam levemente o final do jogo dependendo de quantos são coletados. Além disso, existem fitas cassetes que desbloqueiam variações mais difíceis de "B-Side" de certos níveis, e corações de cristal usados ​​para acessar o conteúdo pós-jogo. Vencendo todos os "B-Sides", em seguida, desbloqueia as versões "C-Side", que consiste em muito duras, mas pequenas variações sobre os níveis. Ao limpar todos os "C-Sides", o jogador pode acessar o menu Variantes. O menu Variantes permite que os jogadores alterem a física do jogo de maneira similar ao Modo de Assistência. Algumas dessas configurações "variantes" incluem: acelerar o jogo, 360 graus e baixa fricção em todas as superfícies planas. Essas configurações servem para tornar o jogo mais desafiador ou mais divertido. O protótipo original do "Celeste Classic" para o console Pico-8 também pode ser encontrado como um minigame escondido.</p>
     </div>
-    <div class="col-lg-4 col-12 col-md-12">
+    <div class="imagens-projeto col-lg-4 col-12 col-md-12">
       <img class="mb-4 img-fluid" src="../img/img_projeto/cenario.png" alt="Imagem 1">
       <img class="mb-4 img-fluid" src="../img/img_projeto/cenario2.png"alt="Imagem 2">
       <img class="img-fluid" src="../img/img_projeto/cenario3.png" alt="Imagem 3">
